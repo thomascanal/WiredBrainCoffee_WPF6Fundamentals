abe15dde822b0f558db1721f702040d5c95e54ab
Thomas Canal <trrc@gmx.ch>
2022-04-26 07:52:44 +0200

    Create Setup

---
+++ WiredBrainCoffee.CustomersApp/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace WiredBrainCoffee.CustomersApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonAddCustomer_Click(object sender, RoutedEventArgs e)
+        {
+            btnAddCustomer.Content = "Customer added!";
+        }
+    }
+}
