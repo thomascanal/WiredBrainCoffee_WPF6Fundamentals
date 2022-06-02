de11e4490acf44e8d8cab5cd479e14e06d56b880
Thomas Canal <trrc@gmx.ch>
2022-06-02 08:31:41 +0200

    Applaying Data Bindung and MVVM - Create a CustomersViewModel

---
+++ WiredBrainCoffee.CustomersApp/ViewModel/CustomersViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+using WiredBrainCoffee.CustomersApp.Data;
+using WiredBrainCoffee.CustomersApp.Model;
+
+namespace WiredBrainCoffee.CustomersApp.ViewModel
+{
+    public class CustomersViewModel
+    {
+        private readonly ICustomerDataProvider _customerDataProvider;
+
+        public CustomersViewModel(ICustomerDataProvider customerDataProvider)
+        {
+            _customerDataProvider = customerDataProvider;
+        }
+        public ObservableCollection<Customer> Customers { get; } = new();
+
+        public async Task LoadAsync()
+        {
+            if (Customers.Any())
+            {
+                return;
+            }
+
+            var customers = await _customerDataProvider.GetAllAsync();
+            if (customers is not null)
+            {
+                foreach (var customer in customers)
+                {
+                    Customers.Add(customer);
+                }
+            }
+        }
+    }
+}
