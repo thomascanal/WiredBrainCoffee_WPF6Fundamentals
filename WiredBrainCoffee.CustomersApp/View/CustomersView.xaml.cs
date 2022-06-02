04801566f7cb7d9df40e187b797cc994c5d9cf6a
Thomas Canal <trrc@gmx.ch>
2022-06-02 08:40:31 +0200

    Applaying Data Bindung and MVVM - Use the ViewModel for the CustomersView

--- WiredBrainCoffee.CustomersApp/View/CustomersView.xaml.cs
+++ WiredBrainCoffee.CustomersApp/View/CustomersView.xaml.cs
@@ -1,13 +1,25 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+using WiredBrainCoffee.CustomersApp.Data;
+using WiredBrainCoffee.CustomersApp.ViewModel;
 
 namespace WiredBrainCoffee.CustomersApp.View
 {
     public partial class CustomersView : UserControl
     {
+        private CustomersViewModel _viewModel;
+
         public CustomersView()
         {
             InitializeComponent();
+            _viewModel = new CustomersViewModel(new CustomerDataProvider());
+            DataContext = _viewModel;
+            Loaded += CustomerView_Loaded;
+        }
+
+        private async void CustomerView_Loaded(object sender, RoutedEventArgs e)
+        {
+            await _viewModel.LoadAsync();
         }
 
         private void ButtonMoveNavigation_Click(object sender, RoutedEventArgs e)
