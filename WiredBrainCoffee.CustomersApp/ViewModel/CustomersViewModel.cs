dfa09e6daf24c77ff58449c65718c011c3de20da
Thomas Canal <trrc@gmx.ch>
2022-07-07 10:20:49 +0200

    Executing Code with Commands - Raise the Command's CanExecuteChanged Event

--- WiredBrainCoffee.CustomersApp/ViewModel/CustomersViewModel.cs
+++ WiredBrainCoffee.CustomersApp/ViewModel/CustomersViewModel.cs
@@ -18,6 +18,7 @@ namespace WiredBrainCoffee.CustomersApp.ViewModel
             _customerDataProvider = customerDataProvider;
             AddCommand = new DelegateCommand(Add);
             MoveNavigationCommand = new DelegateCommand(MoveNavigation);
+            DeleteCommand = new DelegateCommand(Delete, CanDelete);
         }
 
         public ObservableCollection<CustomerItemViewModel> Customers { get; } = new();
@@ -29,6 +30,7 @@ namespace WiredBrainCoffee.CustomersApp.ViewModel
             {
                 _selectedCustomer = value;
                 RaisePropertyChanged();
+                DeleteCommand.RaiseCanExecuteChanged();
             }
         }
 
@@ -43,8 +45,10 @@ namespace WiredBrainCoffee.CustomersApp.ViewModel
         }
 
         public DelegateCommand AddCommand { get; }
-        
+
         public DelegateCommand MoveNavigationCommand { get; }
+
+        public DelegateCommand DeleteCommand { get; }
 
         public async Task LoadAsync()
         {
@@ -77,6 +81,17 @@ namespace WiredBrainCoffee.CustomersApp.ViewModel
                 ? NavigationSide.Right
                 : NavigationSide.Left;
         }
+
+        private void Delete(object? parameter)
+        {
+            if (SelectedCustomer is not null)
+            {
+                Customers.Remove(SelectedCustomer);
+                SelectedCustomer = null;
+            }
+        }
+
+        private bool CanDelete(object? parameter) => SelectedCustomer is not null;
     }
 
     public enum NavigationSide
