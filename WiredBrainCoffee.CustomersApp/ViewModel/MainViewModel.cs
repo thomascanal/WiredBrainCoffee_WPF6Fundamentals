a4e4602ad285f0bfd2bff923eedb7225029110b9
Thomas Canal <trrc@gmx.ch>
2022-08-06 13:00:44 +0200

    Working with Data Templates - Create a SelectViewModelCommand

--- WiredBrainCoffee.CustomersApp/ViewModel/MainViewModel.cs
+++ WiredBrainCoffee.CustomersApp/ViewModel/MainViewModel.cs
@@ -1,24 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
+using WiredBrainCoffee.CustomersApp.Command;
 
 namespace WiredBrainCoffee.CustomersApp.ViewModel
 {
     public class MainViewModel : ViewModelBase
     {
         private ViewModelBase? _selectedViewModel;
-        private CustomersViewModel _customersViewModel;
-        private ProductsViewModel _productsViewModel;
 
         public MainViewModel(CustomersViewModel customersViewModel,
             ProductsViewModel productsViewModel)
         {
-            _customersViewModel = customersViewModel;
-            _productsViewModel = productsViewModel;
-            SelectedViewModel = _customersViewModel;
+            CustomersViewModel = customersViewModel;
+            ProductsViewModel = productsViewModel;
+            SelectedViewModel = CustomersViewModel;
+            SelectViewModelCommand = new DelegateCommand(SelectViewModel);
         }
+
         public ViewModelBase? SelectedViewModel
         {
             get => _selectedViewModel;
@@ -29,12 +26,24 @@ namespace WiredBrainCoffee.CustomersApp.ViewModel
             }
         }
 
+        public CustomersViewModel CustomersViewModel { get; }
+
+        public ProductsViewModel ProductsViewModel { get; }
+
+        public DelegateCommand SelectViewModelCommand { get; }
+
         public async override Task LoadAsync()
         {
             if (SelectedViewModel is not null)
             {
                 await SelectedViewModel.LoadAsync();
             }
+        }
+
+        private async void SelectViewModel(object? parameter)
+        {
+            SelectedViewModel = parameter as ViewModelBase;
+            await LoadAsync();
         }
     }
 }
