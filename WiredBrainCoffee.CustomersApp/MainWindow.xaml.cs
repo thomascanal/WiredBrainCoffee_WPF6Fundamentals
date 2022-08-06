478e96078102416f46e655f0c61eeb578adccb5d
Thomas Canal <trrc@gmx.ch>
2022-08-06 12:26:42 +0200

    Working with Data Templates - Bind to the MainViewModel

--- WiredBrainCoffee.CustomersApp/MainWindow.xaml.cs
+++ WiredBrainCoffee.CustomersApp/MainWindow.xaml.cs
@@ -1,15 +1,25 @@
 ﻿using System.Windows;
+using WiredBrainCoffee.CustomersApp.Data;
+using WiredBrainCoffee.CustomersApp.ViewModel;
 
 namespace WiredBrainCoffee.CustomersApp
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly MainViewModel _viewModel;
+
         public MainWindow()
         {
             InitializeComponent();
+            _viewModel = new MainViewModel(
+                new CustomersViewModel(new CustomerDataProvider()));
+            DataContext = _viewModel;
+            Loaded += MainWindow_Loaded;
+        }
+
+        private async void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            await _viewModel.LoadAsync();
         }
     }
 }
--- WiredBrainCoffee.CustomersApp/View/CustomersView.xaml.cs
+++ WiredBrainCoffee.CustomersApp/View/CustomersView.xaml.cs
@@ -1,25 +1,12 @@
-﻿using System.Windows;
-using System.Windows.Controls;
-using WiredBrainCoffee.CustomersApp.Data;
-using WiredBrainCoffee.CustomersApp.ViewModel;
+﻿using System.Windows.Controls;
 
 namespace WiredBrainCoffee.CustomersApp.View
 {
     public partial class CustomersView : UserControl
     {
-        private CustomersViewModel _viewModel;
-
         public CustomersView()
         {
             InitializeComponent();
-            _viewModel = new CustomersViewModel(new CustomerDataProvider());
-            DataContext = _viewModel;
-            Loaded += CustomerView_Loaded;
-        }
-
-        private async void CustomerView_Loaded(object sender, RoutedEventArgs e)
-        {
-            await _viewModel.LoadAsync();
         }
     }
 }
