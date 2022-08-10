48478405e281913f86dbd1b9ed17bbf323408700
Thomas Canal <trrc@gmx.ch>
2022-08-10 08:13:05 +0200

    Setting up Dependency Injection - Set up Dependency Injection

--- WiredBrainCoffee.CustomersApp/App.xaml.cs
+++ WiredBrainCoffee.CustomersApp/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Windows;
 using WiredBrainCoffee.CustomersApp.Data;
 using WiredBrainCoffee.CustomersApp.ViewModel;
 
@@ -6,14 +7,32 @@ namespace WiredBrainCoffee.CustomersApp
 {
     public partial class App : Application
     {
+        private readonly ServiceProvider _serviceProvider;
+
+        public App()
+        {
+            ServiceCollection services = new();
+            ConfigureServices(services);
+            _serviceProvider = services.BuildServiceProvider();
+        }
+
+        private void ConfigureServices(ServiceCollection services)
+        {
+            services.AddTransient<MainWindow>();
+
+            services.AddTransient<MainViewModel>();
+            services.AddTransient<CustomersViewModel>();
+            services.AddTransient<ProductsViewModel>();
+
+            services.AddTransient<ICustomerDataProvider, CustomerDataProvider>();
+        }
+
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
 
-            var mainWindow = new MainWindow(new MainViewModel(
-                new CustomersViewModel(new CustomerDataProvider()),
-                new ProductsViewModel()));
-            mainWindow.Show();
+            var mainWindow = _serviceProvider.GetService<MainWindow>();
+            mainWindow?.Show();
         }
     }
 }
