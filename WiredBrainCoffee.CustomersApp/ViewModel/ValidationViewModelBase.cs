44c12b85e08dd2e4257697dec8089222058f228f
Thomas Canal <trrc@gmx.ch>
2022-08-27 10:32:19 +0200

    Validating User Input - Create a ValidationViewModelBase Class

---
+++ WiredBrainCoffee.CustomersApp/ViewModel/ValidationViewModelBase.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+
+namespace WiredBrainCoffee.CustomersApp.ViewModel
+{
+    public class ValidationViewModelBase : ViewModelBase, INotifyDataErrorInfo
+    {
+        private readonly Dictionary<string, List<string>> _errorsByPropertyName = new();
+
+        public bool HasErrors => _errorsByPropertyName.Any();
+
+        public event EventHandler<DataErrorsChangedEventArgs>? ErrorsChanged;
+
+        public IEnumerable GetErrors(string? propertyName)
+        {
+            return propertyName is not null && _errorsByPropertyName.ContainsKey(propertyName)
+                ? _errorsByPropertyName[propertyName]
+                : Enumerable.Empty<string>();
+        }
+
+        protected virtual void OnErrorsChanged(DataErrorsChangedEventArgs e)
+        {
+            ErrorsChanged?.Invoke(this, e);
+        }
+    }
+}
